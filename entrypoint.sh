376f6bf5b32f5f01d9701408c8e83cd573c74cfc
Aurèle Oulès <aurele@oules.com>
2023-09-03 22:26:49 +0200

    Fixup payload decode

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ git fetch origin pull/$PR_NUM/head && git checkout FETCH_HEAD
 ./test/get_previous_releases.py -b
 ./autogen.sh && ./configure --disable-fuzz --enable-fuzz-binary=no --with-gui=no --disable-zmq --disable-bench BDB_LIBS="-L${BDB_PREFIX}/lib -ldb_cxx-4.8" BDB_CFLAGS="-I${BDB_PREFIX}/include"
 
-echo $PAYLOAD | base64 -d | jq -r .patch | base64 -d > patch
+echo $PAYLOAD | jq -r .patch | base64 -d > patch
 patch -p0 < patch
 
 make -j$(nproc)
